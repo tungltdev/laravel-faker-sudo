04d6f484a062b4cbf27048433ab7cbc2c7ff3127
tungltdev <tunglt.dev@gmail.com>
2019-08-27 10:17:54 +0700

    fix selecte

--- resources/views/user-selector.blade.php
+++ resources/views/user-selector.blade.php
@@ -1,4 +1,4 @@
-<style>.sudoSu{position: fixed; right: 30px; bottom: 30px; z-index: 99999;}.sudoSu__btn{font-size: 1.5em; background: #333; width: 40px; height: 40px; line-height: 40px; color: white; text-align: center; border-radius: 50%;}.sudoSu__btn--hasSudoed{background: #00b067;}.sudoSu__interface{position: absolute; right: 60px; bottom: 8px;}.sudoSu__interface--hasSudoed{background: white; padding: 20px; bottom: 0; border: 1px solid #cecece; border-radius: 5px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1); width: 400px;}.sudoSu__infoLine{margin-bottom: 5px;}.sudoSu__infoLine > span{font-weight: bold;}.sudoSu__resetBtn{-webkit-appearance: none; -moz-appearance: none; cursor: pointer; border: 0; margin: 5px 0 15px 0; padding: 6px 10px; background: #00b067; color: white; border-radius: 4px; border-bottom: 2px solid #088b54;}.sudoSu__resetBtn:hover{background: #088b54;}.hidden{display: none;}</style>
+<style>.sudoSu__interface .select_user{max-width:150px}.sudoSu__interface--hasSudoed .select_user{width:100%;max-width:unset}.sudoSu{position:fixed;right:20px;bottom:10px;z-index:99999}.sudoSu__btn{font-size:1.5em;background:#333;width:30px;height:30px;line-height:30px;color:#fff;text-align:center;border-radius:50%}.sudoSu__btn--hasSudoed{background:#00b067}.sudoSu__interface{position:absolute;right:36px;bottom:3px}.sudoSu__interface--hasSudoed{background:#fff;padding:20px;bottom:0;border:1px solid #cecece;border-radius:5px;box-shadow:0 1px 1px rgba(0,0,0,.1);width:350px}.sudoSu__infoLine{margin-bottom:5px}.sudoSu__infoLine>span{font-weight:700}.sudoSu__resetBtn{-webkit-appearance:none;-moz-appearance:none;cursor:pointer;border:0;margin:5px 0 15px 0;padding:6px 10px;background:#00b067;color:#fff;border-radius:4px;border-bottom:2px solid #088b54}.sudoSu__resetBtn:hover{background:#088b54}.hidden{display:none}</style>
 
 <div class="sudoSu" title="Sudo Su">
     <div class="sudoSu__btn {{ $hasSudoed ? 'sudoSu__btn--hasSudoed' : '' }}" id="sudosu-js-btn">
@@ -26,7 +26,7 @@
         @endif
 
         <form action="{{ route('fakerUser.login_as_user') }}" method="post">
-            <select name="userId" onchange="this.form.submit()">
+            <select name="userId" onchange="this.form.submit()" class="select_user">
                 <option disabled selected>Faker User</option>
 
                 @foreach ($users as $user)
@@ -36,7 +36,7 @@
 
             {!! csrf_field() !!}
 
-            <input type="hidden" name="originalUserId" value="{{ $originalUser->id ?? null }}">
+            <input type="hidden" name="originalUserId" value="{{ $originalUser->id ?: null }}">
         </form>
     </div>
 </div>
